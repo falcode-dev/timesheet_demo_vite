f5df1617f1cc84d891eef1218975a446bd504879
falcode-dev <falcode.business@gmail.com>
2025-10-18 10:40:48 +0900

    delete QueryClient of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 // src/App.tsx
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { DataverseApp } from "./product/DataverseApp";
 import { UserListProvider } from "./context/UserListContext";
 
-const queryClient = new QueryClient();
-
 /**
  * アプリ全体のルートコンポーネント
- * - React Query Provider を適用
  * - UserListProvider を適用（登録済ユーザーの共有）
  * - DataverseApp（業務UI）を描画
  */
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <UserListProvider>
-        <DataverseApp />
-      </UserListProvider>
-    </QueryClientProvider>
+    <UserListProvider>
+      <DataverseApp />
+    </UserListProvider>
   );
 }
